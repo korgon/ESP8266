a28a852689b7c844fc683fd39654860e57750058
Kevin <kevin@old-devbook.local>
2020-04-06 08:50:17 -0600

    Pushing up stuff so it doesn't get lost.

--- ESP8266 Test/examples/deepsleep.cpp
+++ ESP8266 Test/examples/deepsleep.cpp
@@ -2,21 +2,28 @@
 #define blue 2
 
 void setup() {
-	// put your setup code here, to run once:
+	Serial.begin(115200);
+
 	pinMode(blue, OUTPUT);
 
-	Serial.begin(115200);
 	Serial.println("Light ON");
-	digitalWrite(blue, HIGH);
+	digitalWrite(blue, LOW);
 	
-	delay(30000);
+	delay(3000);	// 3000 = 3s
+
+	digitalWrite(blue, HIGH);
+	Serial.println("Light OFF");
+
+	delay(3000);	// 3000 = 3s
 
 	digitalWrite(blue, LOW);
-	Serial.println("Light OFF");
+	Serial.println("Light ON");
+
+	delay(3000);	// 3000 = 3s
+
+	Serial.println("sleep?");
+	ESP.deepSleep(5000000);	// 5000000 = 5s
 }
 
 void loop() {
-	Serial.println("Begin?");
-	ESP.deepSleep(300000);
-	Serial.println("End?");
 }
--- ESP8266 Test/src/main.cpp
+++ ESP8266 Test/src/main.cpp
@@ -1,20 +1,23 @@
 #include <Arduino.h>
+#include <Ethernet.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+#include "Si7005.h"
+
+
 #define blue 2
 
 void setup() {
 	Serial.begin(115200);
 
+	// turn on blue light...
 	pinMode(blue, OUTPUT);
 
-	Serial.println("Light ON");
-	digitalWrite(blue, LOW);
 	
-	delay(3000);	// 3000 = 3s
 
-	digitalWrite(blue, HIGH);
-	Serial.println("Light OFF");
-
-	Serial.println("sleep?");
+	Serial.println("deep sleep...");
 	ESP.deepSleep(5000000);	// 5000000 = 5s
 }
 
